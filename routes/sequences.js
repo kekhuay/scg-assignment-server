cebf6c9113d57ba7a1cc73011afa93b983bbf4be
Natthaphon Phanit <natthaphon.pha@ascendcorp.com>
2019-06-03 22:00:25 +0700

    add error message

--- routes/sequences.js
+++ routes/sequences.js
@@ -3,8 +3,12 @@ var router = express.Router();
 var linearService = require('../services/linear-equation');
 
 router.post('', function(req, res, next) {
-   var nextThreeSequence = linearService.findNextThreeSequence(req.body.seq);
-   res.send(nextThreeSequence);
+   try {
+     var nextThreeSequence = linearService.findNextThreeSequence(req.body.seq);
+     res.send({nextThreeSequence, msg: ''});
+   } catch {
+     res.send({nextThreeSequence: [], msg: 'could not find solution for this sequence.'});
+   }
 });
 
 module.exports = router;
